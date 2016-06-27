87c4034c4d6583e77c4596e49a748bd0311b65a3
José Atenogenes Bucio Escalante <MacTeno@iMac-de-Jose.local>
2016-06-27 17:32:34 -0500

    Base de datos actualizada, creacion de archivo de newsletter

--- build/php/newletter.php
+++ build/php/newletter.php
@@ -1,4 +1,16 @@
 <?php
-  $email = $_POST['email'];
-  echo $email;
+	require_once("../admin/db/conexion.php");
+	
+	$email = $_POST['email'];
+  
+	$query = "SELECT * FROM Newsletter WHERE Email = '".$email."'";
+	$resultado = mysql_query($query, Conectar::con()) or die(mysql_error());
+	$row = mysql_num_rows($resultado);
+
+	if ($row == 0) {
+		$query1 = "INSERT INTO Newsletter VALUES(null,'".$email."')";
+		$resultado1 = mysql_query($query1, Conectar::con()) or die(mysql_error());
+	} else {
+		echo -1;
+	}
 ?>
