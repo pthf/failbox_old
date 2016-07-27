92bd9b51a11bef282292df32f61cc4b39792e5eb
José Atenogenes Bucio Escalante <MacTeno@iMac-de-Jose.local>
2016-07-27 11:30:05 -0500

    Cambios slider front

--- build/admin/create/register_category.php
+++ build/admin/create/register_category.php
@@ -28,7 +28,7 @@ if ($capital_category == "" || $capital_category == NULL) {
 		$convert_name = explode(' ', $capital_category);
 		$convert_name = strtolower(implode('-', $convert_name));
 		$no_permitidas= array ("á","é","í","ó","ú","Á","É","Í","Ó","Ú","Ñ","ñ","À","Ã","Ì","Ò","Ù","Ã™","Ã ","Ã¨","Ã¬","Ã²","Ã¹","ç","Ç","Ã¢","ê","Ã®","Ã´","Ã»","Ã‚","ÃŠ","ÃŽ","Ã”","Ã›","ü","Ã¶","Ã–","Ã¯","Ã¤","«","Ò","Ã","Ã„","Ã‹");
-		$permitidas= array ("a","e","i","o","u","A","E","I","O","U","N","n","N","A","E","I","O","U","a","e","i","o","u","c","C","a","e","i","o","u","A","E","I","O","U","u","o","O","i","a","e","U","I","A","E","&","/");
+		$permitidas= array ("a","e","i","o","u","A","E","I","O","U","N","n","N","A","E","I","O","U","a","e","i","o","u","c","C","a","e","i","o","u","A","E","I","O","U","u","o","O","i","a","e","U","I","A","E","&");
 		$route_name = strtolower(str_replace($no_permitidas, $permitidas ,$convert_name));
 
 		//registra las categorias de los productos
